9b0b1d98b3d180f4da2151bc801da8a73845e87a
Kevin Bowe <kevinbowe1957@gmail.com>
2016-01-24 18:33:31 -0500

    Code Cleanup - No Functional Change

--- app/scripts/directives/stk-sign-color.js
+++ app/scripts/directives/stk-sign-color.js
@@ -5,20 +5,19 @@ angular.module ('stockDogApp')
     return {
         restrict: 'A',
         link: function ($scope, $element, $attrs) {
+
             // Use $observe to watch expression for changes
             $attrs.$observe('stkSignColor', function (newVal) {
                 var newSign = parseFloat (newVal);
-                // Set elenent's style.color value depending on sign
+                // Set element's style.color value depending on sign
                 if (newSign > 0) {
                     $element[0].style.color = 'Green';
                 } else {
                     $element[0].style.color = 'Red';
                 } // END_If
-                
             }); // END_$Observe
             
         } // END_Link
         
     }; // END_Directive_Return
-    
 }); // END_Directive
--- app/scripts/services/quote-service.js
+++ app/scripts/services/quote-service.js
@@ -17,12 +17,8 @@ angular.module('stockDogApp')
                   var stock = stocks[idx];
                   /*stock.lastPrice = parseFloat(quote.LastTradePriceOnly);*/
                   stock.lastPrice = parseFloat(quote.LastTradePriceOnly) + _.random(-.5, .5);
-                  
                   stock.change = quote.Change;
                   stock.percentChange = quote.ChangeinPercent;
-                  /*stock.change = quote.Change;
-                  stock.percentChange = quote.ChangeinPercent;*/
-                  
                   stock.marketValue = stock.shares * stock.lastPrice;
                   stock.dayChange = stock.shares * parseFloat(stock.change);
                   stock.save();
