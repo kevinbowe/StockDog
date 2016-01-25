ca0f2c86a3c3f98297d0932dbb5392c7c1fbbc75
Kevin Bowe <kevinbowe1957@gmail.com>
2016-01-24 19:35:18 -0500

    Work in progress - Bugs

---
+++ app/scripts/directives/stk-sign-fade.js
@@ -0,0 +1,33 @@
+'use strict';
+
+angular.model('stockDogApp')
+.directive('stkSignFade',function ($animate) {
+    return{
+        restrict: 'A',
+        link: function ($scope, $element, $attrs) {
+            var oldVal = null;
+            // Use $observe to be notified on value changes
+            $attrs.$observe('stkSignFade', function (newVal) {
+                if (oldVal && oldVal == newVal) { return; }
+                
+                var oldPrice = parseFloat(oldVal);
+                var newPrice = parseFloat(newVal);
+                oldVal = newVal;
+                
+                // Add the appropriate direction class, and then remove it
+                if (oldPrice && newPrice) {
+                    var direction = newPrice - oldPrice >= 0 ? 'up' : 'down';
+                    $animate.addClass($element, 'change-' + direction, function() {
+                        $animate.removeClass($element, 'change-' + direction);
+                    });
+                    
+                } // END_If
+                
+            }) // END_$Observe
+            
+            
+        } // END_Link
+        
+    }; // END_Directive_Return
+    
+}); // END_Directive
